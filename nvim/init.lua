c8b6f5f65950198a1835ff27694c32b98b6229a1
Saleh Zaidan <salehzaidan64@gmail.com>
2026-03-05 05:48:56 +0700

    Add filetype to lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -161,7 +161,7 @@ require("lualine").setup({
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "diff", "diagnostics" },
-    lualine_c = { { "filename", path = 1 } },
+    lualine_c = { { "filename", path = 1 }, "filetype" },
     lualine_x = {},
     lualine_y = { "progress" },
     lualine_z = { "location" },
